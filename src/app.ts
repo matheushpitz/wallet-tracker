f07afeded35a4c98cb20615c4aa9af9f8528717f
matheushpitz <matheushenriquepitz@gmail.com>
2022-07-10 20:59:15 -0300

    Added mongo connection.

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import { config } from 'dotenv';
+import mongoose from 'mongoose';
 import { createAlchemy } from "./integrations/alchemy.integration";
 import { createClient } from "./integrations/discord.integration";
 import { FollowCommandService } from "./services/follow-command.service";
@@ -7,6 +8,7 @@ import { UnfollowCommandService } from './services/unfollow-command.service';
 config();
 
 async function init() {
+    await mongoose.connect(process.env['MONGO_CONNECTION_URL'] as string);
     const alchemy = createAlchemy(process.env['ALCHEMY_URL'] as string);
     const discordClient = await createClient(process.env['DISCORD_TOKEN'] as string, process.env['DISCORD_CLIENT_ID'] as string, [
         FollowCommandService.getCommandObject(),
