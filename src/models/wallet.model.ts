c15080102fe5655e53c71c5f391fa5f4733db058
matheushpitz <matheushenriquepitz@gmail.com>
2022-07-10 23:07:50 -0300

    Added wallet model.

---
+++ src/models/wallet.model.ts
@@ -0,0 +1,42 @@
+import mongoose, { Document, Model } from 'mongoose';
+
+export interface IWalletDocument extends Document {
+    label?: string,
+    address: string,
+    channelId: string,
+    createdAt?: Date,
+    createdBy: string
+}
+
+export interface IWalletModel extends Model<IWalletDocument> {
+    findByAddressAndChannel(address: string, channelId: string): Promise<Array<IWalletDocument>>;
+    findByLabelAndChannel(label: string, channelId: string): Promise<Array<IWalletDocument>>;
+    findByIdentifierAndChannel(identifier: string, channelId: string): Promise<Array<IWalletDocument>>;
+    findAllAddresses(): Promise<Array<IWalletDocument>>;
+}
+
+const walletSchema = new mongoose.Schema({
+    label: String,
+    address: { type: String, required: true },
+    channelId: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now() },
+    createdBy: { type: String, required: true }
+});
+
+walletSchema.statics.findByAddressAndChannel = function(address: string, channelId: string) {
+    return this.find({ address, channelId });
+}
+
+walletSchema.statics.findByLabelAndChannel = function(label: string, channelId: string) {
+    return this.find({ label, channelId });
+}
+
+walletSchema.statics.findByIdentifierAndChannel = function(identifier: string, channelId: string) {
+    return this.find({ channelId, $or: [ { address: identifier }, { label: identifier } ] });
+}
+
+walletSchema.statics.findAllAddresses = function() {
+    return this.find().select({ address: 1 });
+}
+
+export const Wallet = mongoose.model<IWalletDocument, IWalletModel>('Wallet', walletSchema);
