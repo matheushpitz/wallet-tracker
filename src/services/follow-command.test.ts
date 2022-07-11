afd2225218f6759fd94e484b6f0392ca6acc7f47
matheushpitz <matheushenriquepitz@gmail.com>
2022-07-11 07:47:15 -0300

    Added tests for commands.

--- src/services/follow-command.test.ts
+++ src/services/follow-command.test.ts
@@ -1,10 +1,77 @@
+import { Wallet } from "../models/wallet.model";
+import { setupConnection } from "../utils/tests.util";
 import { FollowCommandService } from "./follow-command.service";
 
 describe('Follow Wallet Command', () => {
+
+    setupConnection();
+    let interactionSpy: any;
+
+    beforeEach(async () => {
+        const wallet1 = new Wallet({ address: '123', channelId: '1', createdBy: '1' });
+        const wallet2 = new Wallet({ address: '1234', channelId: '1', createdBy: '1', label: 'myWallet' });
+
+        await wallet1.save();
+        await wallet2.save();
+    });
+
+    beforeEach(() => {
+        interactionSpy = {
+            deferReply: jest.fn(() => Promise.resolve),
+            editReply: jest.fn(() => Promise.resolve),
+        };
+    });
+
     test('getCommandObject should have the right name', () => {
         const follow = FollowCommandService.getCommandObject();
 
         expect(follow.command.name).toBe('followwallet');
         expect(follow.listener).toBeDefined();
-    })
+    });
+
+    test('Should display error "label and address must be different"', async () => {
+        const command = FollowCommandService.getCommandObject();
+
+        await command.listener.onExecute(interactionSpy, { address: '123', label: '123' }, { channelId: '1', userId: '1' });
+
+        expect(interactionSpy.deferReply).toBeCalled();
+        expect(interactionSpy.editReply).toBeCalledWith('Address and label must be different');
+    });
+
+    test('Should display error "This channel is already subscribed to this address"', async () => {
+        const command = FollowCommandService.getCommandObject();
+
+        await command.listener.onExecute(interactionSpy, { address: '123' }, { channelId: '1', userId: '1' });
+
+        expect(interactionSpy.deferReply).toBeCalled();
+        expect(interactionSpy.editReply).toBeCalledWith('This channel is already subscribed to this address');
+    });
+
+    test('Should display error "The label myWallet already exists on this channel."', async () => {
+        const command = FollowCommandService.getCommandObject();
+
+        await command.listener.onExecute(interactionSpy, { address: '12345', label: 'myWallet' }, { channelId: '1', userId: '1' });
+
+        expect(interactionSpy.deferReply).toBeCalled();
+        expect(interactionSpy.editReply).toBeCalledWith('The label myWallet already exists on this channel.');
+    });
+
+    test('Should subscribe', async () => {
+        const command = FollowCommandService.getCommandObject();
+
+        await command.listener.onExecute(interactionSpy, { address: '123456', label: 'myWallet2' }, { channelId: '1', userId: '1' });
+
+        expect(interactionSpy.deferReply).toBeCalled();
+        expect(interactionSpy.editReply).toBeCalledWith('Successfully subscribed to 123456.');
+
+        const result = await Wallet.findByAddressAndChannel('123456', '1');
+
+        expect(result.length).toBe(1);
+
+        const element = result[0];
+        expect(element.address).toBe('123456');
+        expect(element.channelId).toBe('1');
+        expect(element.createdBy).toBe('1');
+        expect(element.label).toBe('myWallet2');
+    });
 });
--- src/services/unfollow-command.test.ts
+++ src/services/unfollow-command.test.ts
@@ -1,10 +1,67 @@
+import { Wallet } from "../models/wallet.model";
+import { setupConnection } from "../utils/tests.util";
 import { UnfollowCommandService } from "./unfollow-command.service";
 
 describe('Unfollow Wallet Command', () => {
+
+    setupConnection();
+    let interactionSpy: any;
+
+    beforeEach(async () => {
+        const wallet1 = new Wallet({ address: '123', channelId: '1', createdBy: '1' });
+        const wallet2 = new Wallet({ address: '1234', channelId: '1', createdBy: '1', label: 'myWallet' });
+
+        await wallet1.save();
+        await wallet2.save();
+    });
+
+    beforeEach(() => {
+        interactionSpy = {
+            deferReply: jest.fn(() => Promise.resolve),
+            editReply: jest.fn(() => Promise.resolve),
+        };
+    });
+
     test('getCommandObject should have the right name', () => {
         const follow = UnfollowCommandService.getCommandObject();
 
         expect(follow.command.name).toBe('unfollowwallet');
         expect(follow.listener).toBeDefined();
-    })
+    });
+
+    test('Should display error "No subscription found for "123456"', async () => {
+        const command = UnfollowCommandService.getCommandObject();
+
+        await command.listener.onExecute(interactionSpy, { identifier: '123456' }, { channelId: '1', userId: '1' });
+
+        expect(interactionSpy.deferReply).toBeCalled();
+        expect(interactionSpy.editReply).toBeCalledWith('No subscription found for "123456"');
+    });
+
+    test('Should display error "No subscription found for "myWallet2"', async () => {
+        const command = UnfollowCommandService.getCommandObject();
+
+        await command.listener.onExecute(interactionSpy, { identifier: 'myWallet2' }, { channelId: '1', userId: '1' });
+
+        expect(interactionSpy.deferReply).toBeCalled();
+        expect(interactionSpy.editReply).toBeCalledWith('No subscription found for "myWallet2"');
+    });
+
+    test('Should unfollow from address', async () => {
+        const command = UnfollowCommandService.getCommandObject();
+
+        await command.listener.onExecute(interactionSpy, { identifier: '1234' }, { channelId: '1', userId: '1' });
+
+        expect(interactionSpy.deferReply).toBeCalled();
+        expect(interactionSpy.editReply).toBeCalledWith('Successfully unsubscribed');
+    });
+
+    test('Should unfollow from label', async () => {
+        const command = UnfollowCommandService.getCommandObject();
+
+        await command.listener.onExecute(interactionSpy, { identifier: 'myWallet' }, { channelId: '1', userId: '1' });
+
+        expect(interactionSpy.deferReply).toBeCalled();
+        expect(interactionSpy.editReply).toBeCalledWith('Successfully unsubscribed');
+    });
 });
